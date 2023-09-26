93c875eef391636b2c2931f04c045dbfbbec00f7
au619496 <201804204@post.au.dk>
2023-09-26 11:27:58 +0200

    Enabled streamin chat at the cost of: - Pricing transparrency (can be added as seperate module) - JSON response with seperate email (can be done with additional prompt)

--- src/background/messages/openChat.ts
+++ src/background/messages/openChat.ts
@@ -1,15 +1,11 @@
-import { new_tab_delay_s } from "constants/delays"
 import { DA_SYSTEM_MESSAGE } from "constants/prompts"
-import {
-  createInitialPrompt,
-  PostPrompt,
-  type ApiMessage
-} from "src/services/useOpenAI"
+import { createInitialPrompt, type ApiMessage } from "src/services/useOpenAI"
 
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 import { Storage } from "@plasmohq/storage"
 
-const storage = new Storage()
+const local_storage = new Storage({ area: "local" })
+const sync_storage = new Storage({ area: "sync" })
 
 const goToChat = () => {
   chrome.tabs.create({
@@ -25,13 +21,13 @@ const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
     return
   }
 
-  const api_key = await storage.get("APIKey")
+  const api_key = await sync_storage.get("APIKey")
   if (!api_key) {
     res.send({ error: "missing_api_key" })
     return
   }
 
-  const userCV = await storage.get("user-cv")
+  const userCV = await local_storage.get("user-cv")
   if (!userCV) {
     res.send({ error: "missing_cv" })
     return
@@ -51,27 +47,26 @@ const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   ]
 
   // Send initial prompt to API
-  let response = ""
-  try {
-    response = await PostPrompt(messages, api_key)
-  } catch (e) {
-    if (e.message.includes("Invalid API key")) {
-      res.send({ error: "invalid_api_key" })
-    } else {
-      console.error(e)
-      res.send({ error: "unknown_error" })
-    }
-    return
-  }
+  // try {
+  //   response = await PostPrompt(messages, api_key)
+  // } catch (e) {
+  //   if (e.message.includes("Invalid API key")) {
+  //     res.send({ error: "invalid_api_key" })
+  //   } else {
+  //     console.error(e)
+  //     res.send({ error: "unknown_error" })
+  //   }
+  //   return
+  // }
 
   // Save response to storage and go to chat
   // messages.push({ role: "assistant", content: response })
-  await storage.set("system-messeages", { role: "ChatGPT", content: response })
+  await local_storage.set("message-history", messages)
 
   res.send({ status: "success" })
-  setTimeout(() => {
-    goToChat()
-  }, new_tab_delay_s * 1000)
+  //setTimeout(() => {
+  goToChat()
+  //}, new_tab_delay_s * 1000)
   return
 }
 
