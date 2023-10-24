ccd2c3b261695ae2047fa6653153388fcdf1c36d
Omar Suleiman <omarg@live.dk>
2023-10-24 07:15:29 +0200

    Restored options in svelte

---
+++ src/options.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { apiKey } from "~lib/stores/apiKey";
+
+  let tempAPIkey = "";
+  apiKey.subscribe((value) => {
+    tempAPIkey = value;
+  });
+</script>
+
+<div>
+  <h1>Indsæt din openAI API nøgle for at bruge denne chrome-extension</h1>
+  <input
+    type=""
+    bind:value={tempAPIkey}
+    placeholder="API nøgle"
+    on:blur={() => {
+      apiKey.set(tempAPIkey);
+    }}
+  />
+</div>
+
+<!-- TODO: Insert API key check -->
