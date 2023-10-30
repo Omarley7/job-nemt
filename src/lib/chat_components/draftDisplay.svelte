7f3ea0fb4136ed204d7336d82c8b03aa7ad41746
Omar Suleiman <omarg@live.dk>
2023-10-30 10:37:07 +0100

    Everything is now Svelte New jobChat layout has been created. Removing all old react pages. We are now using daisyUI with tailwind for custom designs.

---
+++ src/lib/chat_components/draftDisplay.svelte
@@ -0,0 +1,11 @@
+<script>
+  import CardBody from "~lib/chat_components/cardBody.svelte";
+</script>
+
+<CardBody>
+  <textarea
+    class="jn-textarea jn-textarea-lg jn-textarea-primary jn-h-full"
+    placeholder="the full text of the draft will be displayed here"
+    disabled
+  />
+</CardBody>
---
+++ src/lib/chat_components/draftPreview.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import CardBody from "~lib/chat_components/cardBody.svelte";
+
+  export let title = "";
+  export let draft = "";
+</script>
+
+<CardBody>
+  <h2 class="jn-card-title">{title}</h2>
+  <p>{draft}...</p>
+</CardBody>
