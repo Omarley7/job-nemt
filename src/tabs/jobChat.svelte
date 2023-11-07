8060f54694c8c05c556b289117b41bf6d88ab586
Omar Suleiman <omarg@live.dk>
2023-11-07 01:22:31 +0100

    Introducing performance issues. Radio buttons added and option to select different jobchat previews.

--- src/tabs/jobChat.svelte
+++ src/tabs/jobChat.svelte
@@ -12,17 +12,27 @@
   const openAIport = getPort("openAI");
   let applicationDetails: JobApplicationDetails;
 
+  let drafts = ["", "", ""];
+  let selectedDraft = 0;
   let postingID: string | null;
+  let loading: boolean = true;
+  let loadingText = "Loading...";
 
-  function readIncomingMessage(chunks: [string, string, string]) {
-    // loading false
+  function readIncomingMessage(
+    chunks: [string, string, string] | { error: string }
+  ) {
+    if ("error" in chunks) {
+      loadingText = chunks.error;
+      return;
+    }
+    loading = false;
     // if chunks[0, 1 and 2] === "null"
     // -- Append message to applicationDetails.messages
     // -- Close connection to Port.
     // else
-    if (chunks) {
+    if (Array.isArray(chunks)) {
       for (let i = 0; i < chunks.length; i++) {
-        generatedDrafts[i].draft += chunks[i];
+        drafts[i] += chunks[i];
       }
     }
   }
@@ -41,7 +51,6 @@
   }
 
   onMount(async () => {
-    //chrome.tabs.onRemoved.addListener(removeActiveStatus);
     applicationDetails = await getApplicationDetails();
 
     // If no messages stored, send prompt to OpenAI. //TODO: Add some loading?
@@ -61,32 +70,46 @@
     }
   });
 
-  let generatedDrafts = [
-    { title: "Draft 1", draft: "" },
-    { title: "Draft 2", draft: "" },
-    { title: "Draft 3", draft: "" },
-  ];
-
   let notes = ["Note 1", "Note 2", "Note 3"];
 </script>
 
-<h1 class="jn-text-center jn-text-xl jn-font-bold">Annonce ID: {postingID}</h1>
-<div
-  class="jn-p-6 custom-grid jn-gap-6
- jn-h-screen jn-items-stretch"
->
-  <!-- Top section with cards -->
-  {#each generatedDrafts as draft}
-    <div class="jn-min-h-32">
-      <DraftPreview draft={draft.draft.substring(0, 500)} title={draft.title} />
+{#if loading}
+  <div
+    class="jn-flex jn-flex-col jn-items-center jn-justify-center jn-h-screen"
+  >
+    <div class="jn-spinner jn-spinner-primary" />
+    <p class="jn-text-center jn-text-lg jn-font-bold jn-mt-4">{loadingText}</p>
+  </div>
+{/if}
+
+<div class="jn-p-6 custom-grid jn-gap-6 jn-h-screen jn-items-stretch">
+  <h1 class="jn-text-center jn-text-xl jn-font-bold jn-col-span-full">
+    Annonce ID: {postingID}
+  </h1>
+  {#each drafts as draft, i}
+    <div
+      class="jn-min-h-32 jn-w-full"
+      tabindex="0"
+      on:click={() => (selectedDraft = i)}
+      on:keydown={(e) => {
+        if (e.key === "Enter" || e.key === " ") selectedDraft = i;
+      }}
+      role="button"
+      aria-label={`Draft ${i + 1}`}
+    >
+      <DraftPreview
+        draft={draft.substring(0, 500)}
+        title={`Draft: ${i + 1}`}
+        selected={selectedDraft === i}
+      />
     </div>
   {/each}
 
   <div class="jn-col-span-3 jn-h-full">
-    <DraftDisplay text={generatedDrafts[0].draft} />
+    <DraftDisplay text={drafts[selectedDraft]} />
   </div>
 
-  <div class="jn-col-start-4 jn-row-start-1 jn-row-end-3">
+  <div class="jn-col-start-4 jn-row-start-2 jn-row-end-4">
     <NoteList {notes} />
   </div>
 
@@ -99,8 +122,8 @@
 <style>
   .custom-grid {
     display: grid;
-    grid-template-rows: 2fr 4fr 0.75fr;
-    grid-template-columns: 1fr 1fr 1fr 30rem;
+    grid-template-rows: 2rem 2fr 4fr 0.5fr;
+    grid-template-columns: 1fr 1fr 1fr 25rem;
     /**TODO: Configure xl, lg, md, sm, xs. Need variable last col.*/
   }
 </style>
