110bd1d6dfa36e2b770896cbdf03ec13f00e9999
Omar Suleiman <omarg@live.dk>
2023-11-22 21:36:18 +0100

    Nested grid

--- src/lib/threeDrafts.svelte
+++ src/lib/threeDrafts.svelte
@@ -1,93 +1,110 @@
-<script lang="ts" context="module">
+<script lang="ts">
+  import { onMount } from "svelte"
+
+  import { getPort } from "@plasmohq/messaging/port"
+  import { Storage } from "@plasmohq/storage"
+
   import DraftDisplay, {
-    throttleScrollToBottom,
-  } from "~lib/chat_components/draftDisplay.svelte";
-  import DraftPreview from "~lib/chat_components/draftPreview.svelte";
+    throttleScrollToBottom
+  } from "~lib/chat_components/draftDisplay.svelte"
+  import DraftPreview from "~lib/chat_components/draftPreview.svelte"
+  import type { JobApplicationDetails } from "~types"
+
   //TODO: Update to only scroll to bottom when the user is at the bottom of the textarea
   //Enable when onscroll shows user is a bottom
   //Disable when user scrolls up
-  export const scrollToBottom = throttleScrollToBottom;
-</script>
+  const scrollToBottom = throttleScrollToBottom
 
-<script lang="ts">
-  import { getPort } from "@plasmohq/messaging/port";
-  import { Storage } from "@plasmohq/storage";
-  import type { JobApplicationDetails } from "~types";
-  import { onMount } from "svelte";
+  let clazz: string = ""
+  export { clazz as class }
+  export let applicationDetails: JobApplicationDetails
+  let drafting: boolean
+  let drafts: [string, string, string] = ["", "", ""]
+  let postingID: string
 
-  export let applicationDetails: JobApplicationDetails;
-  console.log(applicationDetails);
-  let drafting: boolean;
-  let drafts: [string, string, string] = ["", "", ""];
-  let postingID: string;
-
-  let selectedDraft = 0;
-  const localStorageService = new Storage({ area: "local" });
-  const openAIport = getPort("openAI");
+  let selectedDraft = 0
+  const localStorageService = new Storage({ area: "local" })
+  const openAIport = getPort("openAI")
 
   onMount(() => {
-    postingID = applicationDetails.jobPostingID;
+    postingID = applicationDetails.jobPostingID
     if (!applicationDetails.initialDrafts) {
-      openAIport.onMessage.addListener(readThreeDrafts);
+      openAIport.onMessage.addListener(readThreeDrafts)
 
       openAIport.postMessage({
         body: {
           type: "first draft",
-          jobDescription: applicationDetails.jobDescription,
-        },
-      });
+          jobDescription: applicationDetails.jobDescription
+        }
+      })
     } else {
-      drafts = applicationDetails.initialDrafts;
-      drafting = false;
+      drafts = applicationDetails.initialDrafts
+      drafting = false
     }
     return () => {
-      openAIport.disconnect();
-    };
-  });
+      openAIport.disconnect()
+    }
+  })
 
   function readThreeDrafts(
     chunks: [string, string, string] | { error: string } | { DONE: true }
   ) {
-    if ("error" in chunks) throw new Error(chunks.error);
+    if ("error" in chunks) throw new Error(chunks.error)
 
     // loadingText = undefined;
-    drafting = true;
+    drafting = true
 
     if ("DONE" in chunks) {
-      applicationDetails.initialDrafts = drafts;
-      localStorageService.set(postingID, applicationDetails);
-      openAIport.disconnect();
-      drafting = false;
+      applicationDetails.initialDrafts = drafts
+      localStorageService.set(postingID, applicationDetails)
+      openAIport.disconnect()
+      drafting = false
     }
 
     if (Array.isArray(chunks)) {
       for (let i = 0; i < chunks.length; i++) {
-        drafts[i] += chunks[i];
+        drafts[i] += chunks[i]
       }
-      scrollToBottom(); //Could be moved to component.
+      scrollToBottom() //Could be moved to component.
     }
   }
 </script>
 
-{#each drafts as draft, i}
-  <div
-    class="jn-min-h-32 jn-w-full"
-    tabindex="0"
-    on:click={() => (selectedDraft = i)}
-    on:keydown={(e) => {
-      if (e.key === "Enter" || e.key === " ") selectedDraft = i;
-    }}
-    role="button"
-    aria-label={`Draft ${i + 1}`}
-  >
-    <DraftPreview
-      draft={draft.substring(0, 750)}
-      title={`Draft: ${i + 1}`}
-      selected={selectedDraft === i}
-    />
-  </div>
-{/each}
+<div class="multi-drafts jn-gap-6 ${clazz}">
+  {#each drafts as draft, i}
+    <div
+      class="jn-min-h-16 jn-w-full"
+      tabindex="0"
+      on:click={() => (selectedDraft = i)}
+      on:keydown={(e) => {
+        if (e.key === "Enter" || e.key === " ") selectedDraft = i
+      }}
+      role="button"
+      aria-label={`Draft ${i + 1}`}>
+      <DraftPreview
+        draft={draft.substring(0, 750)}
+        title={`Draft: ${i + 1}`}
+        selected={selectedDraft === i} />
+    </div>
+  {/each}
 
-<div class="jn-col-span-3 jn-h-full">
-  <DraftDisplay text={drafts[selectedDraft]} isDisabled={drafting} />
+  <div class="jn-h-full preview">
+    <DraftDisplay text={drafts[selectedDraft]} isDisabled={drafting} />
+  </div>
 </div>
+
+<style>
+  .multi-drafts {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-areas:
+      ". preview"
+      ". preview"
+      ". preview";
+  }
+
+  .preview {
+    grid-area: preview;
+  }
+</style>
--- src/tabs/jobChat.svelte
+++ src/tabs/jobChat.svelte
@@ -1,69 +1,67 @@
 <script lang="ts">
-  import "~/style.css";
-  import NoteList from "~lib/chat_components/noteList.svelte";
-  import { Storage } from "@plasmohq/storage";
-  import type { JobApplicationDetails } from "~types";
-  import ThreeDrafts from "~lib/threeDrafts.svelte";
+  import "~/style.css"
+
+  import { Storage } from "@plasmohq/storage"
+
+  import NoteList from "~lib/chat_components/noteList.svelte"
+  import ThreeDrafts from "~lib/threeDrafts.svelte"
+  import type { JobApplicationDetails } from "~types"
 
   // I think we need to add a dispatch error from child to parent
   window.onerror = function (message, source, lineno, colno, error) {
-    console.log("Caught error: ", message, source, lineno, colno, error);
-    loadingText = "Error: " + message;
-  };
+    console.log("Caught error: ", message, source, lineno, colno, error)
+    loadingText = "Error: " + message
+  }
 
   // We COULD add a store for this... But no reactivity from local storage
   // Since we don't want to add support between Extension components
-  const localStorageService = new Storage({ area: "local" });
-  let applicationDetails: JobApplicationDetails;
+  const localStorageService = new Storage({ area: "local" })
+  let applicationDetails: JobApplicationDetails
 
-  let notes: string[] = [];
-  let postingID: string;
-  let loadingText: string | undefined = "Loading...";
+  let notes: string[] = []
+  let postingID: string
+  let loadingText: string | undefined = "Loading..."
 
   async function getApplicationDetails(): Promise<JobApplicationDetails> {
     postingID =
-      new URLSearchParams(window.location.search).get("postingID") ?? "";
+      new URLSearchParams(window.location.search).get("postingID") ?? ""
 
     if (postingID === "") {
-      loadingText = "No postingID found in query params";
-      throw new Error(loadingText);
+      loadingText = "No postingID found in query params"
+      throw new Error(loadingText)
     }
 
-    applicationDetails = await localStorageService.get(postingID);
+    applicationDetails = await localStorageService.get(postingID)
 
     if (!applicationDetails) {
-      loadingText = "No application details found in local storage";
-      throw new Error(loadingText);
+      loadingText = "No application details found in local storage"
+      throw new Error(loadingText)
     }
-    loadingText = undefined;
-    return applicationDetails;
+    loadingText = undefined
+    return applicationDetails
   }
 </script>
 
 {#if loadingText !== undefined}
   <div
-    class="jn-flex jn-flex-col jn-items-center jn-justify-center jn-h-screen"
-  >
+    class="jn-flex jn-flex-col jn-items-center jn-justify-center jn-h-screen">
     <div class="jn-spinner jn-spinner-primary" />
     <p class="jn-text-center jn-text-lg jn-font-bold jn-mt-4">{loadingText}</p>
   </div>
 {/if}
 
-<div
-  class="jn-pt-4 jn-pb-4 jn-pl-6 jn-pr-6 custom-grid jn-gap-6 jn-h-screen jn-items-stretch"
->
-  <h1 class="jn-text-center jn-text-xl jn-font-bold jn-col-span-full">
+<div class="jn-pt-4 jn-pb-4 jn-pl-6 jn-pr-6 custom-grid jn-gap-6 jn-h-screen">
+  <h1 class="jn-text-center jn-text-xl jn-font-bold header">
     Annonce ID: {postingID}
   </h1>
   {#await getApplicationDetails() then applicationDetails}
-    <ThreeDrafts {applicationDetails} />
+    <ThreeDrafts {applicationDetails} class="main" />
   {/await}
-  <div class="jn-col-start-4 jn-row-start-2 jn-row-end-4">
+  <div class="notes">
     <NoteList {notes} />
   </div>
 
-  <!-- Submit button -->
-  <div class="jn-col-span-4 jn-h-2">
+  <div class="submit-button">
     <button class="jn-btn jn-btn-primary jn-w-full"> Lav din ansøgning </button>
   </div>
 </div>
@@ -71,8 +69,28 @@
 <style>
   .custom-grid {
     display: grid;
-    grid-template-rows: 1.75rem minmax(12rem, 0.75fr) minmax(4rem, 2fr) 3.2rem;
-    grid-template-columns: 1fr 1fr 1fr 25rem;
+    grid-template-rows: 1.75rem minmax(4rem, 2fr) 3.2rem;
+    grid-template-columns: 1fr 25rem;
+    grid-template-areas:
+      "header header"
+      "main notes"
+      "submit submit";
     /**TODO: Configure xl, lg, md, sm, xs. Need variable last col.*/
+  }
+
+  .submit-button {
+    grid-area: submit;
+  }
+
+  .header {
+    grid-area: header;
+  }
+
+  .notes {
+    grid-area: notes;
+  }
+
+  .main {
+    grid-area: main;
   }
 </style>
